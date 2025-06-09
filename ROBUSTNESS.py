c8f36ce162c1ce411b2fa431468053eb0639c65c
Alvaro Molina <molinayunez@hotmail.com>
2025-06-09 17:15:30 -0500

    Add backtest scripts and data (excluding large CSV)

--- ROBUSTNESS.py
+++ ROBUSTNESS.py
@@ -3,11 +3,9 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+pd.set_option("display.max_rows", None)
 
-df = pd.read_csv("SUMMARY OF PARAMETER SWEEP(LOOKBACKS-HOLDING PERIODS) MINUTES.csv")
-print(df)
+data = pd.read_csv("SUMMARY_SWEEP_LOOKBACK_HOLD_SL_TP.csv")
+data = data.drop('Unnamed: 0',axis=1)
+print(data)
 
-pivot = df.pivot(index="lookback", columns="holding", values="sharpe")
-sns.heatmap(pivot, cmap="viridis", annot=True, fmt=".1f")
-plt.title("Sharpe Ratio Heatmap")
-plt.show()
